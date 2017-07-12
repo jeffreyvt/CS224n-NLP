fefd021d3fc2577fee0f1cedb02d64e367d9714d
jeffrey <jeffrey.vt@hotmail.com>
2017-07-12 13:09:41 +1000

    partially completed assignment 1

--- assignment1/get_datasets.sh
+++ assignment1/get_datasets.sh
@@ -7,18 +7,18 @@ cd $DATASETS_DIR
 
 # Get Stanford Sentiment Treebank
 if hash wget 2>/dev/null; then
-  wget http://nlp.stanford.edu/~socherr/stanfordSentimentTreebank.zip
+  wget https://nlp.stanford.edu/~socherr/stanfordSentimentTreebank.zip
 else
-  curl -O http://nlp.stanford.edu/~socherr/stanfordSentimentTreebank.zip
+  curl -O https://nlp.stanford.edu/~socherr/stanfordSentimentTreebank.zip
 fi
 unzip stanfordSentimentTreebank.zip
 rm stanfordSentimentTreebank.zip
 
 # Get 50D GloVe vectors
 if hash wget 2>/dev/null; then
-  wget http://nlp.stanford.edu/data/glove.6B.zip
+  wget https://nlp.stanford.edu/data/glove.6B.zip
 else
-  curl -O http://nlp.stanford.edu/data/glove.6B.zip
+  curl -O https://nlp.stanford.edu/data/glove.6B.zip
 fi
 unzip glove.6B.zip
 rm glove.6B.zip
